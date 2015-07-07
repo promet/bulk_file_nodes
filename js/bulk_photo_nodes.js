55936164054fe9911ed3c23dc0ffdf2a5ab796bb
John Ross <johnross@prometsource.com>
2015-07-08 01:32:15 +0800

    Hiding loading gif on a per request method.
    ajaxStop does not fire up for very long requests, especially for
    requests that goes beyond timeout. Added ajaxComplete to add
    reaction on a per request bases.

--- js/bulk_photo_nodes.js
+++ js/bulk_photo_nodes.js
@@ -10,7 +10,10 @@ jQuery(document).ready(function($) {
     .ajaxStart(function(){
       $(this).append('<div class="ajax-overlay" /><div class="ajax-progress-throbber" />')
     })
-    .ajaxStop(function () {
+    .ajaxComplete(function(){
+      $('.ajax-overlay, .ajax-progress-throbber', $(this)).remove();
+    })
+    .ajaxStop(function(){
       $('.ajax-overlay, .ajax-progress-throbber', $(this)).remove();
     });
 });
